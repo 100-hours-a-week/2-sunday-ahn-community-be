940201cda71cb3fbfc2879683d80bf7bf71cdc9d
goorm <goorm@goormui-MacBookPro-5.local>
2024-12-14 19:23:56 +0900

    feat: DB연결 connection->pool로 변경

--- config/database.js
+++ config/database.js
@@ -1,15 +1,18 @@
-import 'dotenv/config'; // dotenv를 import 방식으로 불러옴
+import 'dotenv/config'; 
 import mysql from 'mysql2';
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 100, // 최대 연결 수
+    queueLimit: 0, // 대기열 제한 (0은 제한 없음)
 });
 
-connection.connect(err => {
+connection.getConnection(err => {
     if (err) {
         console.error('데이터베이스 연결실패 :'.red, err.stack);
         return;
