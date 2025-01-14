ac55524b1d0b2db326bcd70cd64facaacd6ed392
goorm <goorm@goormui-MacBookPro-5.local>
2025-01-14 15:52:39 +0900

    feauture:CDN설정추가

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 import 'dotenv/config'; 
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
 const connection = mysql.createPool({
     host: process.env.DB_HOST,
@@ -7,17 +7,22 @@ const connection = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    timezone: 'Asia/Seoul', // 문자열로 타임존 설정
     waitForConnections: true,
     connectionLimit: 100, // 최대 연결 수
     queueLimit: 0, // 대기열 제한 (0은 제한 없음)
 });
 
-connection.getConnection(err => {
-    if (err) {
-        console.error('데이터베이스 연결실패 :'.red, err.stack);
-        return;
+// 연결 테스트
+(async () => {
+    try {
+        const conn = await connection.getConnection(); // Promise 기반으로 연결
+        console.log('데이터베이스 연결 완료'.green);
+        conn.release(); // 연결 해제
+    } catch (err) {
+        console.error('데이터베이스 연결 실패 :'.red, err.stack);
     }
-    console.log('데이터베이스 연결 완료'.green);
-});
+})();
 
-export default connection;
+
+export default {connection};
